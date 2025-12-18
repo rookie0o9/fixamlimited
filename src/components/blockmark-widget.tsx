35df192a4754b1c052b79565edc442c0ee34b891
rookie0o9 <mycybsecprofade@gmail.com>
2025-12-18 06:02:08 +0000

    Update Cyber Essentials widget embed

--- src/components/blockmark-widget.tsx
+++ src/components/blockmark-widget.tsx
@@ -5,7 +5,7 @@ const BlockmarkWidget = () => {
     <section className="bg-background py-10">
       <div className="flex justify-center">
         <iframe
-          src="https://registry.blockmarktech.com/certificates/17029d52-efbe-4ca6-b4ba-bd2f5b34d18a/widget/?tooltip_position=bottom&theme=transparent"
+          src="https://registry.blockmarktech.com/certificates/17029d52-efbe-4ca6-b4ba-bd2f5b34d18a/widget/?tooltip_position=bottom_right&theme=transparent&hover=t"
           className="blockmark-widget"
           title="Cyber Essentials / Blockmark certificate"
           loading="lazy"
